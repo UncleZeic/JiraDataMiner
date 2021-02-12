81929a5cdebbd4265894f318564c5764a7c723cc
UncleZeic <41185719+UncleZeic@users.noreply.github.com>
2021-02-12 19:46:27 +0200

    Update setup.py
    Connection data

--- setup.py
+++ setup.py
@@ -3,5 +3,4 @@ from credential import Credential
 
 def get_credentials():
     credential = Credential("<jira_url>", "usr", "<pwd>")
-    credential = Credential("https://jira.telenav.com:8443/", "marian.zeic@telenav.com", "w3e2;'w3e2")
     return credential
