310960630ba9cdaceef27d2a001effa84078a82b
Marius Magureanu <marius.magureanu@dnb.no>
2021-02-15 12:43:24 +0100

    A couple of cosmetical updates in main

--- main.py
+++ main.py
@@ -20,21 +20,31 @@ def parse_input_args():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-    args = parse_input_args()
-
-    jiraAccess = JiraAccess(args.hostname, args.username, args.password)
+def get_resolved_story_points(j_a):
     resolved_story_points_past_weeks = []
 
     for wi in range(-1, -__number_of_weeks, -1):
-        resolved_issues_in_week = jiraAccess.get_resolved_in_week(
+        resolved_issues_in_week = j_a.get_resolved_in_week(
             __atlas_board_filter_id, wi)
 
-        resolvedStoryPointsInWeek = sum(JiraAccess.get_story_points(
+        resolved_story_points_in_week = sum(j_a.get_story_points(
             issue) or 0 for issue in resolved_issues_in_week)
-        resolved_story_points_past_weeks.append(resolvedStoryPointsInWeek)
+        resolved_story_points_past_weeks.append(resolved_story_points_in_week)
 
+    return resolved_story_points_past_weeks
+
+
+def go_figure(stories):
     fig = go.Figure()
-    fig.add_trace(go.Bar(y=resolved_story_points_past_weeks))
+    fig.add_trace(go.Bar(y=stories))
     fig.update_layout(title='Hello Figure')
     fig.show()
+
+
+if __name__ == '__main__':
+    args = parse_input_args()
+
+    jira_acc = JiraAccess(args.hostname, args.username, args.password)
+    resolved_stories = get_resolved_story_points(jira_acc)
+
+    go_figure(stories)
